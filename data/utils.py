b742d0d106e86d4f57f6dda4e98dbe9e92247510
mdgoldberg <mgoldberg@college.harvard.edu>
2016-01-19 14:31:58 -0500

    updated utils

--- data/utils.py
+++ data/utils.py
@@ -6,3 +6,17 @@ def getDHM(raw_timestamp, index=['day', 'hour', 'minute']):
     hour = raw_timestamp[-4:-2]
     minute = raw_timestamp[-2:]
     return pd.Series([day, hour, minute], index=index)
+
+def makeTrainCSV(fn):
+    df = pd.read_csv(fn)
+    df = df.replace(-1, np.nan)
+    df.columns = [c.strip() for c in df.columns]
+    df.rename(columns={'Timestamp (DHHMM)': 'raw_timestamp'}, inplace=True)
+    times = df.raw_timestamp.astype(str).apply(getDHM).astype(int)
+    df = pd.concat((df, times), axis=1)
+    df['minutes'] = df['hour'] * 60 + df['minute']
+    df['weekday'] = df['day'] % 7
+    return df
+
+def makeTestCSV(fn):
+    pass
