a4ebf62bae023bec472d23e95d7a810dc889fbbb
mdgoldberg <mgoldberg@college.harvard.edu>
2016-01-20 03:42:58 -0500

    updated goldberg

--- goldberg.py
+++ goldberg.py
@@ -26,21 +26,23 @@ sens_dummies = pd.get_dummies(newdf.sensor).iloc[:, :-1]
 newdf = pd.concat((newdf, day_dummies, sens_dummies), axis=1)
 comp_df = newdf.dropna()
 Xdf = comp_df.ix[:, ['Xcoord', 'Ycoord', 'hour', 'neighbor_avg', 'isRestroom', 'isStaircase'] +
-                 [c for c in comp_df.columns if c in day_dummies.columns or c in sens_dummies.columns]]
+                 list(day_dummies.columns)
+                 # or c in sens_dummies.columns
+                 ]
 Y = comp_df.value.copy()
 
-# model = ensemble.RandomForestRegressor(n_estimators=200, n_jobs=-1)
-# grid = {'max_features': ['auto', 'sqrt', 'log2', 0.25, 0.5, 0.75]}
-# model = GridSearchCV(model, grid, refit=True, n_jobs=-1, verbose=2)
-model = ensemble.GradientBoostingRegressor(n_estimators=200, loss='huber', max_depth=5)
-grid = {'learning_rate': [0.05, 0.1, 0.15], 'alpha': [0.9, 0.95, 0.99]}
+# model = ensemble.RandomForestRegressor(n_jobs=-1)
+# grid = {'n_estimators': [100, 150, 200], 'max_features': ['auto', 'sqrt', 'log2', 0.4, 0.5, 0.6]}
+model = ensemble.GradientBoostingRegressor(loss='huber', n_estimators=150, max_depth=5)
+grid = {'learning_rate': [0.05, 0.1, 0.15, 0.2], 'alpha': [0.8, 0.85, 0.9, 0.95, 0.99]}
 model = GridSearchCV(model, grid, refit=True, n_jobs=-1, verbose=2)
 Xtrain, Xtest, ytrain, ytest = train_test_split(Xdf, Y, test_size=0.1)
 print 'fitting...'
-model.fit(Xtrain, ytrain)
-cv_preds = model.predict(Xtest)
-absErrs = np.abs(cv_preds - ytest).values
-print np.mean(absErrs), np.median(absErrs)
+# model.fit(Xtrain, ytrain)
+# cv_preds = model.predict(Xtest)
+# absErrs = np.abs(cv_preds - ytest).values
+# print np.mean(absErrs), np.median(absErrs)
+model.fit(Xdf, Y)
 
 day_dummies = pd.get_dummies(testdf.start_weekday, prefix='day').iloc[:, :-1]
 sens_dummies = pd.get_dummies(testdf.sensor).iloc[:, :-1]
@@ -51,7 +53,7 @@ testSensors = newtest.sensor.str[1:].astype(int) - 1
 newtest['neighbor_avg'] = [neighbor_df.ix[h, s] for h, s in zip(testHours, testSensors)]
 comp_test = newtest.dropna()
 Xtest = comp_test.ix[:, ['Xcoord', 'Ycoord', 'start_hour', 'neighbor_avg', 'isRestroom', 'isStaircase'] + 
-                        [c for c in comp_test.columns if c in day_dummies.columns or c in sens_dummies.columns]]
+                        [c for c in comp_test.columns if c in day_dummies.columns]]# or c in sens_dummies.columns]]
 
 preds = model.predict(Xtest)
 preds = [p if p >= 0 else 0 for p in preds]
