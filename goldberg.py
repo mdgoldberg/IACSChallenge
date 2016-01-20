53e870532ee637ee30c192436e01f29b949bfc9c
mdgoldberg <mgoldberg@college.harvard.edu>
2016-01-20 02:47:12 -0500

    combine gbr and rfr into goldberg.py

--- goldberg.py
+++ goldberg.py
@@ -16,21 +16,25 @@ neighbor_df = pd.DataFrame(neighbor_avgs,
 # cut = pd.cut(df2.minutes, xrange(-1, 1441, 30), labels=range(48))
 # df2['bucket'] = cut.get_values()
 newdf = df2.groupby(['day', 'weekday', 'hour', 'sensor', 'isRestroom', 'isStaircase']).value.sum().reset_index()
-hours = (newdf['day'] - 1) * 24 + newdf['hour']
+hours = map(int, (newdf['day'] - 1) * 24 + newdf['hour'])
 sensors = newdf['sensor'].str[1:].astype(int) - 1
 newdf['neighbor_avg'] = [neighbor_df.ix[h, s] for h, s in zip(hours, sensors)]
 newdf = utils.addSensorColumns(newdf)
-dummies = pd.get_dummies(newdf.weekday, prefix='day').iloc[:, :-1]
-newdf = pd.concat((newdf, dummies), axis=1)
+newdf = utils.addDummyRows(newdf, 'hour')
+day_dummies = pd.get_dummies(newdf.weekday, prefix='day').iloc[:, :-1]
+sens_dummies = pd.get_dummies(newdf.sensor).iloc[:, :-1]
+newdf = pd.concat((newdf, day_dummies, sens_dummies), axis=1)
 comp_df = newdf.dropna()
 Xdf = comp_df.ix[:, ['Xcoord', 'Ycoord', 'hour', 'neighbor_avg', 'isRestroom', 'isStaircase'] +
-                 [c for c in comp_df.columns if c in dummies.columns]]
+                 [c for c in comp_df.columns if c in day_dummies.columns or c in sens_dummies.columns]]
 Y = comp_df.value.copy()
 
 # model = ensemble.RandomForestRegressor(n_estimators=200, n_jobs=-1)
+# grid = {'max_features': ['auto', 'sqrt', 'log2', 0.25, 0.5, 0.75]}
+# model = GridSearchCV(model, grid, refit=True, n_jobs=-1, verbose=2)
 model = ensemble.GradientBoostingRegressor(n_estimators=200, loss='huber', max_depth=5)
-grid = {'learning_rate': [0.15, 0.2, 0.25, 0.3], 'alpha': [0.8, 0.85, 0.9, 0.95, 0.99]}
-model = GridSearchCV(model, grid, refit=True, n_jobs=-1)
+grid = {'learning_rate': [0.05, 0.1, 0.15], 'alpha': [0.9, 0.95, 0.99]}
+model = GridSearchCV(model, grid, refit=True, n_jobs=-1, verbose=2)
 Xtrain, Xtest, ytrain, ytest = train_test_split(Xdf, Y, test_size=0.1)
 print 'fitting...'
 model.fit(Xtrain, ytrain)
@@ -38,17 +42,19 @@ cv_preds = model.predict(Xtest)
 absErrs = np.abs(cv_preds - ytest).values
 print np.mean(absErrs), np.median(absErrs)
 
-testHours = (testdf['start_day'] - 1) * 24 + testdf['start_hour']
-testSensors = testdf.sensor.str[1:].astype(int) - 1
-testdf['neighbor_avg'] = [neighbor_df.ix[h, s] for h, s in zip(testHours, testSensors)]
-newtest = testdf.ix[:, ['start_day', 'start_weekday', 'start_hour', 'Xcoord', 'Ycoord', 'neighbor_avg', 'isRestroom', 'isStaircase']]
-dummies = pd.get_dummies(newtest.start_weekday, prefix='day').iloc[:, :-1]
-newtest = pd.concat((newtest, dummies), axis=1)
+day_dummies = pd.get_dummies(testdf.start_weekday, prefix='day').iloc[:, :-1]
+sens_dummies = pd.get_dummies(testdf.sensor).iloc[:, :-1]
+newtest = pd.concat((testdf, day_dummies, sens_dummies), axis=1)
+newtest = newtest.ix[~newtest.isDummy]
+testHours = map(int, (newtest['start_day'] - 1) * 24 + newtest['start_hour'])
+testSensors = newtest.sensor.str[1:].astype(int) - 1
+newtest['neighbor_avg'] = [neighbor_df.ix[h, s] for h, s in zip(testHours, testSensors)]
 comp_test = newtest.dropna()
 Xtest = comp_test.ix[:, ['Xcoord', 'Ycoord', 'start_hour', 'neighbor_avg', 'isRestroom', 'isStaircase'] + 
-                        [c for c in comp_test.columns if c.startswith('day_')]]
+                        [c for c in comp_test.columns if c in day_dummies.columns or c in sens_dummies.columns]]
 
 preds = model.predict(Xtest)
+preds = [p if p >= 0 else 0 for p in preds]
 fi = pd.Series(dict(zip(Xtest.columns, model.best_estimator_.feature_importances_))).sort_values(ascending=True)
 print fi
 print model.best_estimator_
